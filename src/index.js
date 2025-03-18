f9982f857395420c1485163f4f1ca89d746ac321
Fzgt <fzgt320@gmail.com>
2025-03-18 14:46:19 +1100

    test minSize

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import data from './data.js'
 // import from这些ESM规范，Webpack有jsloader会自己处理。
+import $ from 'jquery'
 
-console.log(data)
+console.log($)
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
-const { runtime } = require("webpack");
-
 module.exports = {
     entry: {
         main: './src/index.js',
         app: './src/app.js'
     },
+
     optimization: {
         runtimeChunk: {
             name: 'runtime'
-        }
-    }
+        },
+        splitChunks: {
+            chunks: 'all',
+            cacheGroups: {},
+            // minSize: {
+            //     javascript: 30000,
+            //     style: 0
+            // },
+            // maxSize: {
+            //     javascript: 0,
+            //     style: 0
+            // }
+        },
+    },
 }; 
