16d22c489a3205fb25bd8b21457fdb98e9c6af46
Fzgt <fzgt320@gmail.com>
2025-03-21 21:41:37 +1100

    maxAsyncRequest控制拆分几个异步chunk,但是不生效

--- webpack.config.js
+++ webpack.config.js
@@ -12,19 +12,20 @@ module.exports = {
         },
         splitChunks: {
             chunks: 'all',
+            maxAsyncRequests: 1, // 最多拆分出几个异步chunk
             cacheGroups: {
-                // commons: {
-                //     chunks: 'all',
-                //     name: 'chunk-commons',
-                //     minChunks: 2,
-                //     requestExistingChunk: true,
-                // }
+                commons: {
+                    chunks: 'all',
+                    name: 'chunk-commons',
+                    minChunks: 2,
+                    // requestExistingChunk: true,
+                }
             },
-            // minSize: {
+            // minSize: { // 小于某个值就没必要拆包
             //     javascript: 30000,
             //     style: 0
             // },
-            // maxSize: {
+            // maxSize: { // 超过某个值就要拆包
             //     javascript: 0,
             //     style: 0
             // }
