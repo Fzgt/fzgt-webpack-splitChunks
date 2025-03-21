3fc89b191ac6583616cb81e7a072afd1a4ba05d3
Fzgt <fzgt320@gmail.com>
2025-03-21 23:42:59 +1100

    Performance可以拦截组内成员打包的的体积,同时也避免使用maxSize

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,11 @@ module.exports = {
     //     main: './src/index.js',
     //     app: './src/app.js'
     // },
-
+    performance: {
+        maxAssetSize: 250000, // 最大资源大小250kb
+        maxEntrypointSize: 250000, // 最大入口资源大小250kb
+        hints: 'warning'
+    },
     optimization: {
         runtimeChunk: {
             name: 'runtime'
@@ -17,16 +21,20 @@ module.exports = {
                 commons: {
                     chunks: 'all',
                     name: 'chunk-commons',
-                    minChunks: 2,
+                    minChunks: 2, // 当一个文件被引用两次以上，就会放到chunk-commons里
                     // requestExistingChunk: true,
-                }
+                },
+                vendors: {}
             },
+            // minSize maxSize优先级太高，会导致cacheGroups失效
+            // splitChunks难在单entry很难控制，多entry的时候配置更容易生效
+
             // minSize: { // 小于某个值就没必要拆包
             //     javascript: 30000,
             //     style: 0
             // },
             // maxSize: { // 超过某个值就要拆包
-            //     javascript: 0,
+            //     javascript: 120000,
             //     style: 0
             // }
         },
